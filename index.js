9a60c2e4e20c8b153863d6980099a07fd0c248b9
Manuel Estrada <estradamanuel15@yahoo.com>
2024-08-27 17:22:28 -0300

    segundo com

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ let frutas = ["uva", "manzana", "pera", "naranja", "frutilla"]
 
 let Numeros = [1,2,3,4,5]
 let NumDup = []
-    for (i=0; i<Numeros.length; i++)
-    {let num = Numeros[i]*2
+    for (i=1; i<Numeros.length + 1; i++)
+    {let num = i*2
         NumDup.push(num)
     }
     
@@ -45,3 +45,66 @@ let dobles = Numeros.map(Element => Element * 2)
 
 let colores = ["rojo", "amarillo", "azul"]
 let colores2 = ["violeta", "verde"]
+
+let todos = colores.concat(colores2)
+console.log(todos)
+
+//1
+
+let Libro = {
+    titulo: "mi libro luna de pluton",
+    autor: "Juan :D",
+    ano: 2020
+}
+
+console.log(Libro)
+console.table(Libro)
+
+//1.B
+
+Persona.hobby = "airsoft"
+Persona.edad = 27
+console.log(Persona)
+
+//1.C
+
+let Calculadora = {
+    suma: function (x,y){
+        return x + y
+    },
+
+    resta: function(x,y){
+        return x - y
+    },
+
+    multi: function(x,y){
+        return x * y
+    },
+
+    divi: function(x,y){
+        return x / y
+    }
+}
+
+//2.A
+Persona.profesion = "ser malo"
+let Temp = `nombre: ${Persona.nombre} edad: ${Persona.edad} prof: ${Persona.profesion}`
+console.log(Temp)
+
+//2.B
+let Producto = {
+    precio: 400,
+    nombrePro: "Iphone"
+}
+function frase (nombrePro, precio){
+   return `producto: ${nombrePro} precio: ${precio}`
+}
+console.log(frase(Producto.nombrePro, Producto.precio))
+
+//2.C
+
+let ERROR = {
+    nombre: "caca",
+    desc: "mucho texto"
+}
+console.log(`Nombre Error: ${ERROR.nombre}, desc: ${ERROR.desc}`)
