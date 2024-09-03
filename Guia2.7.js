70dc8c39454ded02528596db12e5c085eb8419e5
Manuel Estrada <estradamanuel15@yahoo.com>
2024-09-03 17:23:00 -0300

    menuda mierda

--- Guia2.7.js
+++ Guia2.7.js
@@ -1,29 +1,18 @@
-function obtenerRequisitosDesdeEnlace(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json(); // Asume que el contenido es JSON
-            })
-            .then(data => {
-                resolve(data); // Devuelve los datos obtenidos
-            })
-            .catch(error => {
-                reject(error); // Maneja cualquier error
-            });
-    });
+async function listoParaViajar() {
+    try{
+        let response = await fetch("https://www.mockachino.com/33754ea7-2586-48/preparativos");
+        let data = await response.json()
+        let dataLimpia = data.preparativos
+
+        let dataFiltrada = dataLimpia.filter(function(x){
+            return x.valor === false
+        })
+
+        let listoResultado = dataFiltrada.map((x) => `${x.requisito}`).join(", ")
+        console.log("Requisitos faltantes: ", listoResultado)
+
+        }catch(error){
+            console.log(error)
+        }
 }
-
-const enlace = 'https://www.mockachino.com/33754ea7258648/preparativos';
-
-obtenerRequisitosDesdeEnlace(enlace)
-    .then(data => {
-        console.log('Requisitos obtenidos:', data);
-        // Aquí puedes procesar los datos obtenidos, por ejemplo:
-        // Puedes realizar alguna operación con los datos obtenidos, como filtrarlos, procesarlos, etc.
-    })
-    .catch(error => {
-        console.error('Error al obtener los requisitos:', error);
-    });
+listoParaViajar("https://www.mockachino.com/33754ea7-2586-48/preparativos")
